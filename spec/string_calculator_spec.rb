6bd91846659da8cd8f8c8f4e2d2a37ec141a422c
prinapatel2211 <prinapatel.2211@gmail.com>
2025-03-10 22:31:28 +0530

    Added Spec and code for sum of empty String returns 0

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../app/string_calculator'
 
 RSpec.describe StringCalculator do
-  
+
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      calculator = StringCalculator.new
+      expect(calculator.add("")).to eq(0)
+    end
+  end
 end
