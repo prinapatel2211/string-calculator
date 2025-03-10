4027dae21a144335b3a302e419508879077cfc28
prinapatel2211 <prinapatel.2211@gmail.com>
2025-03-10 22:33:36 +0530

    Rspec and code for single and multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,11 +1,19 @@
 require_relative '../app/string_calculator'
 
 RSpec.describe StringCalculator do
+  subject { StringCalculator.new }
 
   describe '#add' do
     it 'returns 0 for an empty string' do
-      calculator = StringCalculator.new
-      expect(calculator.add("")).to eq(0)
+      expect(subject.add("")).to eq(0)
+    end
+
+    it 'returns the number itself when there is a single number' do
+      expect(subject.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of numbers separated by commas' do
+      expect(subject.add("1,5")).to eq(6)
     end
   end
 end
